d3d27b7f36eda57586ec0663aec821c8edfd5450
Fhejs7g <adudaniel097@gmail.com>
2023-08-08 16:46:30 -0700

    fixed

--- Portfolio-website/index.js
+++ Portfolio-website/index.js
@@ -131,25 +131,3 @@ anchorLinks.forEach(anchor => {
     }
   });
 });
-
-const imagesToLazyLoad = document.querySelectorAll('img[data-src]');
-
-const options = {
-  root: null, 
-  rootMargin: '0px',
-  threshold: 0.2 
-};
-
-const observer = new IntersectionObserver((entries, observer) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      const img = entry.target;
-      img.src = img.dataset.src; 
-      observer.unobserve(img); 
-    }
-  });
-}, options);
-
-imagesToLazyLoad.forEach(img => {
-  observer.observe(img);
-});
